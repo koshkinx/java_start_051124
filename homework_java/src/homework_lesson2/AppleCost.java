9e6e015dacd8e5b74c4fc4f00a15db9edab9ce9d
Dmytro Senchuk <koshkinx9@gmail.com>
2024-11-08 19:41:18 +0200

    Initial commit

---
+++ homework_java/src/homework_lesson2/AppleCost.java
@@ -0,0 +1,15 @@
+package homework_lesson2;
+
+public class AppleCost {
+    public static void main(String[] args) {
+
+        int AppleCost = 2;
+        int Cost = 6;
+        int totalSum;
+
+        totalSum = Cost * AppleCost;
+
+        System.out.println(totalSum + " $");
+
+    }
+}
---
+++ homework_java/src/homework_lesson2/Gerona.java
@@ -0,0 +1,13 @@
+package homework_lesson2;
+
+public class Gerona {
+	public static void main(String[] args) {
+
+	double sideA = 0.3;
+	double sideB = 0.4;
+	double sideC = 0.5;
+	double p = (sideA+sideB+sideC) / 2;
+	double area = Math.sqrt(p * (p - sideA)*(p - sideB) * (p - sideC));
+	System.out.printf("Площадь треугольника: %.2f", area);
+}
+}
